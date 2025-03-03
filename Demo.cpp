ee7ab3fc8be87b4df8dea292d238fd71ff5ba1b5
unknown <binyaminsaldman@gmail.com>
2025-03-03 08:15:23 +0200

    added demo and tests

---
+++ Demo.cpp
@@ -0,0 +1,38 @@
+#include "graph.hpp"
+#include "algorithms.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    graphs::Graph g(5); 
+    g.addEdge(0, 1, 1);
+    g.addEdge(0, 4, 1);
+    g.addEdge(1, 2, 1);
+    g.addEdge(1, 3, 1);
+    g.addEdge(1, 4, 1);
+    g.addEdge(2, 3, 1);
+    g.addEdge(3, 4, 1);
+
+    g.printGraph(); // Should print something like:
+    /*
+    Adjacency list of vertex 0: (4, 1) -> (1, 1) -> NULL
+    Adjacency list of vertex 1: (4, 1) -> (3, 1) -> (2, 1) -> (0, 1) -> NULL
+    Adjacency list of vertex 2: (3, 1) -> (1, 1) -> NULL
+    Adjacency list of vertex 3: (4, 1) -> (2, 1) -> (1, 1) -> NULL
+    Adjacency list of vertex 4: (3, 1) -> (1, 1) -> (0, 1) -> NULL
+    */
+    
+    cout << "BFS starting from vertex 0:" << endl;
+    algos::Algorithms::BFS(g, 0); // Should print 0 4 1 3 2 
+    
+    cout << "DFS starting from vertex 0:" << endl;
+    algos::Algorithms::DFS(g, 0); // Should print 0 4 3 2 1
+    
+    cout << "Dijkstra from 0 to 3:" << endl;
+    algos::Algorithms::Dijkstra(g, 0, 3); // Should print something like: Shortest path from 0 to 3 is: 0->4->3 with weight 2
+    
+    cout << algos::Algorithms::hasCycle(g) << endl; // Should print true
+    
+    return 0;
+}
---
+++ Test.cpp
@@ -0,0 +1,19 @@
+#include "doctest.h"
+#include "Algorithms.hpp"
+#include "Graph.hpp"
+
+using namespace std;
+
+TEST_CASE("Example")
+{
+    graphs::Graph g(5); 
+    g.addEdge(0, 1, 1);
+    g.addEdge(0, 4, 1);
+    g.addEdge(1, 2, 1);
+    g.addEdge(1, 3, 1);
+    g.addEdge(1, 4, 1);
+    g.addEdge(2, 3, 1);
+    g.addEdge(3, 4, 1);
+    CHECK(algos::Algorithms::hasCycle(g) == true)
+}
+
