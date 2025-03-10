2bbcde2f6f8cf30c67908d9e93dce2d43d66e4e4
BenjaminSaldman <93070344+BenjaminSaldman@users.noreply.github.com>
2025-03-10 07:21:26 +0200

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -25,18 +25,18 @@ int main() {
     */
     
     cout << "BFS starting from vertex 0:" << endl;
-    vector<int> bfs = Algorithms::BFS(g, 0); 
+    vector<int> bfs = Algorithms::bfs_scan(g, 0); 
     Algorithms::print_algorithm_result(bfs) // Should print 0 4 1 3 2 
     
     cout << "DFS starting from vertex 0:" << endl;
-    vector<int> dfs = algos::Algorithms::DFS(g, 0); 
+    vector<int> dfs = algos::Algorithms::dfs_scan(g, 0); 
     Algorithms::print_algorithm_result(dfs) // Should print 0 4 3 2 1
     
     cout << "Dijkstra from 0 to 3:" << endl;
-    vector<int> dijkstra = algos::Algorithms::Dijkstra(g, 0, 3); 
+    vector<int> dijkstra = algos::Algorithms::dijkstra_path(g, 0, 3); 
     Algorithms::print_algorithm_result(dijkstra, true) // Should print something like: Shortest path from 0 to 3 is: 0->4->3 with weight 2
         
-    cout << algos::Algorithms::hasCycle(g) << endl; // Should print true
+    cout << algos::Algorithms::has_cycle(g) << endl; // Should print true
     
     return 0;
 }
